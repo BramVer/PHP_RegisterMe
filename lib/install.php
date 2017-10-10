c17deeab6f5b07ed7c5e00ad1b2f560af8f98984
Bram <bramvereertbrugghen@live.be>
2017-10-10 15:02:12 +0200

    Copied install files to ensure nothing goes wrong here. To avoid the case of accidental typos

--- lib/install.php
+++ lib/install.php
@@ -1,129 +1,149 @@
 <?php
+// Load the hashing library from the root of this project
+require_once getRootPath() . '/vendor/password_compat/lib/password.php';
 
-  /**
-   * Blog installer function
-   *
-   * @return array(count array, error string)
-   */
-  function installBlog(PDO $pdo)
-  {
-    // Get a couple of useful project paths
-    $root = getRootPath();
-    $database = getDatabasePath();
+/**
+ * Blog installer function
+ *
+ * @return array(count array, error string)
+ */
+function installBlog(PDO $pdo)
+{
+	// Get a couple of useful project paths
+	$root = getRootPath();
+	$database = getDatabasePath();
 
-    $error = '';
+	$error = '';
 
-    // A security measure, to avoid anyone resetting the DB if it exists
-    if (is_readable($database) && filesize($database) > 0)
-      $error = 'Please delete existing DB manually before reinstalling.';
+	// A security measure, to avoid anyone resetting the database if it already exists
+	if (is_readable($database) && filesize($database) > 0)
+	{
+		$error = 'Please delete the existing database manually before installing it afresh';
+	}
 
-    // Create an empty file for the DB
-    if(!$error)
-    {
-      $createdOk = @touch($database);
+	// Create an empty file for the database
+	if (!$error)
+	{
+		$createdOk = @touch($database);
+		if (!$createdOk)
+		{
+			$error = sprintf(
+				'Could not create the database, please allow the server to create new files in \'%s\'',
+				dirname($database)
+			);
+		}
+	}
 
-      if(!$createdOk)
-        $error = sprintf('Could not create DB, give server permission to edit location ' . dirname($database));
-    }
+	// Grab the SQL commands we want to run on the database
+	if (!$error)
+	{
+		$sql = file_get_contents($root . '/data/init.sql');
 
-    // Grab the SQL commands we want to run on the DB
-    if(!$error)
-    {
-      $sql = file_get_contents($root . '/data/init.sql');
+		if ($sql === false)
+		{
+			$error = 'Cannot find SQL file';
+		}
+	}
 
-      if($sql === false)
-        $error = 'Cannot find SQL file.';
-    }
+	// Connect to the new database and try to run the SQL commands
+	if (!$error)
+	{
+		$result = $pdo->exec($sql);
+		if ($result === false)
+		{
+			$error = 'Could not run SQL: ' . print_r($pdo->errorInfo(), true);
+		}
+	}
 
-    // Connect to the new DB and try to run the SQL commands
-    if(!$error)
-    {
-      $result = $pdo -> exec($sql);
+	// See how many rows we created, if any
+	$count = array();
 
-      if($result === false)
-        $error = 'Could not run SQL: ' . print_r($pdo -> errorInfo(), true);
-    }
+	foreach(array('post', 'comment') as $tableName)
+	{
+		if (!$error)
+		{
+			$sql = "SELECT COUNT(*) AS c FROM " . $tableName;
+			$stmt = $pdo->query($sql);
+			if ($stmt)
+			{
+				// We store each count in an associative array
+				$count[$tableName] = $stmt->fetchColumn();
+			}
+		}
+	}
 
-    // See how many rows we created, if any
-    $count = array();
+	return array($count, $error);
+}
 
-    foreach(array('post', 'comment') as $tableName)
-    {
-      if(!$error)
-      {
-        $sql = "SELECT COUNT(*) AS c FROM " . $tableName;
-        $stmt = $pdo -> query($sql);
+/**
+ * Updates the admin user in the database
+ *
+ * @param PDO $pdo
+ * @param string $username
+ * @param integer $length
+ * @return array Duple of (password, error)
+ */
+function createUser(PDO $pdo, $username, $length = 10)
+{
+	// This algorithm creates a random password
+	$alphabet = range(ord('A'), ord('z'));
+	$alphabetLength = count($alphabet);
 
-        if($stmt)
-          $count[$tableName] = $stmt -> fetchColumn();
-      }
-    }
+	$password = '';
+	for($i = 0; $i < $length; $i++)
+	{
+		$letterCode = $alphabet[rand(0, $alphabetLength - 1)];
+		$password .= chr($letterCode);
+	}
 
-    return array($count, $error);
-  }
+	$error = '';
 
-  /**
-   * Creates a new user in the database
-   *
-   * @param PDO $pdo
-   * @param string $username
-   * @param integer $length
-   * @return array Duple of (password, error)
-   */
-  function createUser(PDO $pdo, $username, $length = 10)
-  {
-    // Creates random pwd
-    $alphabet = range(ord('A'), ord('Z'));
-    $alphabetLength = count($alphabet);
+	// Insert the credentials into the database
+	$sql = "
+		UPDATE
+			user
+		SET
+			password = :password, created_at = :created_at, is_enabled = 1
+		WHERE
+			username = :username
+	";
+	$stmt = $pdo->prepare($sql);
+	if ($stmt === false)
+	{
+		$error = 'Could not prepare the user update';
+	}
 
-    $password = '';
-    for($i = 0; $i < $length; $i++)
-    {
-      $lettercode = $alphabet[rand(0, $alphabetLength - 1)];
-      $password .= chr($lettercode);
-    }
+	if (!$error)
+	{
+		// Create a hash of the password, to make a stolen user database (nearly) worthless
+		$hash = password_hash($password, PASSWORD_BCRYPT);
+		if ($hash === false)
+		{
+			$error = 'Password hashing failed';
+		}
+	}
 
-    $error = '';
+	// Insert user details, including hashed password
+	if (!$error)
+	{
+		$result = $stmt->execute(
+			array(
+				'username' => $username,
+				'password' => $hash,
+				'created_at' => getSqlDateForNow(),
+			)
+		);
+		if ($result === false)
+		{
+			$error = 'Could not run the user password update';
+		}
+	}
 
-    // Insert credentials into db
-    $sql = "INSERT INTO
-            user
-            (username, password, created_at)
-            VALUES
-            (:username, :password, :created_at)";
+	if ($error)
+	{
+		$password = '';
+	}
 
-    $stmt = $pdo -> prepare($sql);
-    if($stmt === false)
-      $error = 'Could not prepare the user creation.';
-
-    //Password encryption
-    if(!$error)
-    {
-      // Create hash of pwd
-      $hash = password_hash($password, PASSWORD_DEFAULT);
-
-      if($hash === false)
-        $error = 'Password hashing failed.';
-    }
-
-    // Insert user details
-    if(!$error)
-    {
-      $result = $stmt -> execute(
-        array(
-          'username' => $username,
-          'password' => $hash,
-          'created_at' => getSQLDateForNow(),
-        )
-      );
-
-      if($result === false)
-        $error = 'Could not run the user creation.';
-    }
-
-    if($error)
-      $password = '';
-
-    return array($password, $error, );
-  }
+	return array($password, $error);
+}
 ?>
